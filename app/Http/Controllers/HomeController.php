4b84aff0ce1ed98ce263194fe32b0a645e368252
Gerson <gedacaba@gmail.com>
2022-07-02 16:29:45 -0500

    configuracion inicial

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+
+    }
+    public function create()
+    {
+
+    }
+    public function show()
+    {
+
+    }
+}
