dfc0faf4365d5f0ba28a3f368c37a471e584e460
Gerson Calvo <gedacaba@gmail.com>
2022-08-02 13:27:14 -0500

    rutas de tipo resourses

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,18 @@ use App\Http\Controllers\CursoController;
 */
 Route::get('/', [CursoController::class, 'index']);
 
-Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
+//rutas de la manera convencional
+/* Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
 Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
 Route::post('cursos',  [CursoController::class, 'store'])->name('cursos.store');
 Route::get('cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
-
 Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
-
 Route::put('cursos/{curso}',  [CursoController::class, 'update'])->name('cursos.update');
+Route::delete('cursos/{curso}',  [CursoController::class, 'destroy'])->name('cursos.destroy'); */
 
-Route::delete('cursos/{curso}',  [CursoController::class, 'destroy'])->name('cursos.destroy');
+
+//rutas de tipo resourses
+Route::resource('cursos', CursoController::class);
 
 
 
